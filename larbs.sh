4bfc317772569d8170c51348dbb915148c01d10a
Lexian <71324166+lLexian@users.noreply.github.com>
2021-04-26 16:49:56 +0000

    removed colour emoji install

--- larbs.sh
+++ larbs.sh
@@ -196,9 +196,6 @@ manualinstall $aurhelper || error "Failed to install AUR helper."
 # and all build dependencies are installed.
 installationloop
 
-dialog --title "LARBS Installation" --infobox "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
-yes | sudo -u "$name" $aurhelper -S libxft-bgra-git >/dev/null 2>&1
-
 # Install the dotfiles in the user's home directory
 putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
 rm -f "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml"
